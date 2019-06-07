f598c23849cfdea604bb3980e0c244861cd63d72
Chris Gaona <chris@specialtacticssolutions.com>
2019-06-07 15:04:25 -0700

    Finds nodes from a shallow rendered component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Test = () => <div>Testing</div>
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='paragraph'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -19,6 +21,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>Welcome to React</h1>
+      <ul className='unordered-list'>
+        <li>Test 1</li>
+        <li>Test 2</li>
+        <li>Test 3</li>
+      </ul>
+      <Test />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() })
+
+describe('<App />', () => {
+  let wrapper = null
+  beforeAll(() => {
+    wrapper = shallow(<App />, { context: {}, disableLifecycleMethods: false })
+  })
+
+  it.skip('should render App', () => {
+    console.log(wrapper.debug())
+  });
+  
+  it('should contain 1 p element', () => {
+    expect(wrapper.find('p').length).toBe(1)
+    expect(wrapper.find('p').exists()).toBe(true)
+    expect(wrapper.find('.paragraph').exists()).toBe(true)
+  });
+
+  it('should contain ul & li elements', () => {
+    expect(wrapper.find('ul').children().length).toBe(3)
+    expect(wrapper.find('ul').hasClass('unordered-list')).toBe(true)
+  });
+
+  it('should have an h1 with text', () => {
+    expect(wrapper.find('h1').text()).toBe('Welcome to React')
+  });
 });
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
